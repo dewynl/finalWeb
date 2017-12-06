4f79655ccea33cc8e78f467f8df4404d233e61c6
Dewyn <dewyn.liriano@gmail.com>
2017-12-06 00:55:37 -0400

    Dominio de usuario refactorizado, departamentos reemplazados por Tipos de Usuarios, correos se envían al crear un usuario.

---
+++ grails-app/domain/finalweb/TipoUsuario.groovy
@@ -0,0 +1,33 @@
+package finalweb
+
+enum TipoUsuario {
+    GENERICO("Normal"),
+    CLIENTE_CONSUMIDOR_FINAL("Consumidor Final"),
+    CLIENTE_EMPRESA("Cliente Empresarial"),
+    CLIENTE_PERSONA_FISICA("Persona Fisica"),
+    VENTAS("Empleado de Ventas"),
+    ALMACEN("Empleado de Almacen"),
+    ADMIN("Administrador")
+
+    final String val
+
+
+
+    TipoUsuario(String value) {
+        this.val = value
+    }
+
+    String toString() {
+        return this.val
+    }
+
+    String getKey() {
+        return this.name()
+    }
+
+    static List<TipoUsuario> getListaTipos(){
+        List lista = [GENERICO, CLIENTE_CONSUMIDOR_FINAL, CLIENTE_EMPRESA, CLIENTE_PERSONA_FISICA, VENTAS, ALMACEN, ADMIN]
+        return lista
+    }
+
+}
